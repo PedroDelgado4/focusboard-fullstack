37496c17bd7ee61a6d9cadf3ddd0a0255e6a785c
Pedro Delgado Durán <fpedrodelgadoduran@gmail.com>
2026-02-24 15:40:12 +0100

    Fix fetch hardcoding issues

--- frontend/src/api/tasks.js
+++ frontend/src/api/tasks.js
@@ -1,5 +1,4 @@
 const API_URL = import.meta.env.VITE_API_URL + "/api/tasks/"
-console.log("API_URL:", API_URL)
 export async function getTasks(token) {
     const response = await fetch(API_URL, {
         headers: {
